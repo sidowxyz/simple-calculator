6dd8e5d84691d668c4f866523ca5a59c3fd6072c
sidowxyz <sidowxyz@yahoo.com>
2024-06-15 20:59:45 +0300

    Updated file

--- calculator.py
+++ calculator.py
@@ -17,8 +17,7 @@ def main():
     print("Select operation:")
     print("1. Add")
     print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
+
 
     while True:
         choice = input("Enter choice (1/2/3/4): ")
